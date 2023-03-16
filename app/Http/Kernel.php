f00c808f9c4c8d6aaea6b4867e395bc2f328f94a
Vilija <vilija.jakubaityte@gmail.com>
2023-03-16 09:40:21 +0200

    Middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Http;
+use App\Http\Middleware\AdminMiddleware;
+use App\Http\Middleware\ShortCodeMiddleware;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -62,5 +64,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin' => AdminMiddleware::class,
+        'short.code' => ShortCodeMiddleware::class,
     ];
 }
---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+       $type=$request->user()->type;
+       //dd($request->user());
+        if ($request->user() !== null) {
+            $type = $request->user()->type;
+            if ($type == 'admin') {
+                return $next($request);
+            } else {
+                return redirect()->route('owners.list');
+            }
+        }
+        else {
+                return redirect()->route('login');
+          }
+
+        return redirect()->route('owners.list');
+        return $next($request);
+
+    }
+
+
+}
+
